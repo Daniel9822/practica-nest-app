58e801063cb6b1648d3381115e3da6c06090c016
Daniel9822 <dhdany64@gmail.com>
2023-09-10 15:11:41 -0400

    add auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,15 +2,19 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto/auth.dto";
 import { AuthCreateDto } from "./dto/auth.dto.create";
+import { Public } from "src/decorator/public.decorator";
 
 @Controller("auth")
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Public()
   @Post("login")
   login(@Body() { email, password }: AuthDto) {
     return this.authService.login({ email, password });
   }
 
+  @Public()
   @Post("register")
   register(@Body() userInfo: AuthCreateDto) {
     return this.authService.registe(userInfo);
