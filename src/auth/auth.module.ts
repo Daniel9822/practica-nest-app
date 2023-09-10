5b9b2baf2734290ed58a8149d54a5a4b515b46be
Daniel9822 <dhdany64@gmail.com>
2023-09-10 15:11:58 -0400

    add auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,20 +5,23 @@ import { AuthController } from "./auth.controller";
 import { JwtModule } from "@nestjs/jwt";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/schema/user.schema";
+import { RoleGuard } from "src/common/role.guard";
+
+export const secret = "djajfjfkklwk123332dkfr";
 
 @Module({
   imports: [
     UserModule,
     JwtModule.register({
       global: true,
-      secret: "djajfjfkklwk123332dkfr",
+      secret,
       signOptions: {
         expiresIn: "1d",
       },
     }),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  providers: [AuthService],
+  providers: [AuthService, RoleGuard],
   controllers: [AuthController],
 })
 export class AuthModule {}
