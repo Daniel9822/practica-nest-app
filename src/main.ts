b5ad273b1052d5c63176a60cf9ed9afa3279942b
Daniel9822 <dhdany64@gmail.com>
2023-09-10 15:12:36 -0400

    update main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { AuthGuard } from "./common/auth.guard";
+import { JwtService } from "@nestjs/jwt";
+import { Reflector } from "@nestjs/core";
+import { RoleGuard } from "./common/role.guard";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalGuards(new AuthGuard(new JwtService(), new Reflector()));
+  app.useGlobalGuards(new RoleGuard(new Reflector()));
   await app.listen(3000);
 }
 bootstrap();
